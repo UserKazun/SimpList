bc839353e6a1c6bb24ce3b3b68b0e379b52301b1
UserKazun <kazuusjlo8@gmail.com>
2021-04-04 15:13:09 +0900

    moreviewのUI、挙動の修正

--- SimpList/ViewModel/ItemViewModel.swift
+++ SimpList/ViewModel/ItemViewModel.swift
@@ -39,7 +39,7 @@ class ItemViewModel: ObservableObject {
     }
     
     func updateItem(_ item: TodoItem, _ title: String, _ startDate: String = "", _ endDate: String = "", _ note: String, _ isDone: Bool) -> TodoItem? {
-        let updatedItem = itemModelStore.updateItem(item.id!, title: title, startDate: startDate, endDate: startDate, note: note, isDone: isDone)
+        let updatedItem = itemModelStore.updateItem(item.id!, title: title, startDate: startDate, endDate: endDate, note: note, isDone: isDone)
         items = itemModelStore.items
         
         return updatedItem
@@ -58,45 +58,6 @@ class ItemViewModel: ObservableObject {
     
     func toggleIsDone(_ item: TodoItem) {
         self.itemModelStore.toggleIsDone(item)
-    }
-    
-    func formattedDateForUserData(inputDate: Date) -> String {
-        let dateFormat = DateFormatter()
-        dateFormat.dateStyle = .medium
-        dateFormat.timeStyle = .medium
-        dateFormat.dateFormat = "yyyy/M/d E HH:mm"
-        dateFormat.timeZone = timeZone
-        let dateString = dateFormat.string(from: inputDate)
-        
-        return dateString
-    }
-    
-    func formattedDateForDisplayItem(inputDate: String) -> Date {
-        let dateFormat = DateFormatter()
-        dateFormat.dateStyle = .medium
-        dateFormat.timeStyle = .medium
-        dateFormat.dateFormat = "yyyy-MM-dd E"
-        dateFormat.timeZone = timeZone
-        let date = dateFormat.date(from: inputDate)
-        
-        return (date)!
-    }
-    
-    func formattedDateForHeader() -> String {
-        let dateFormat = DateFormatter()
-        dateFormat.dateStyle = .medium
-        dateFormat.timeStyle = .medium
-        
-        if TimeZone(identifier: "America/Halifax") == timeZone {
-            dateFormat.dateFormat = "E MM-dd-yyyy"
-        } else {
-            dateFormat.dateFormat = "yyyy-MM-dd E"
-        }
-        
-        dateFormat.timeZone = timeZone
-        let dateString = dateFormat.string(from: Date())
-        
-        return dateString
     }
     
     static func previewViewModel() -> ItemViewModel {
